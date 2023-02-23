c79439c752b68e01f58dfdebdb16bedb5f207008
Alexandros Maragkakis <alexm280470@gmail.com>
2023-02-23 23:34:06 +0200

    Update youtube_downloader.py

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -1,10 +1,22 @@
-import sys
+import argparse
 from pytube import YouTube
 
-url = sys.argv[1]
-yt = YouTube(url)
+def download_video(url, output_path=None):
+    try:
+        yt = YouTube(url)
+        stream = yt.streams.get_highest_resolution()
 
-print(yt.streams.filter(progressive=True))
+        
 
-stream = yt.streams.get_highest_resolution()
-stream.download()
+        print(f"Downloading video: {yt.title} ({stream.resolution})")
+        stream.download()
+        print("Video downloaded successfully!")
+    except Exception as e:
+        print(f"Error: {str(e)}")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Download a video from YouTube.")
+    parser.add_argument("url", help="YouTube video URL")
+    args = parser.parse_args()
+
+    download_video(args.url)
